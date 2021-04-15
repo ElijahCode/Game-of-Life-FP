6a38ea9f2a5282132c7603166e88d5414b7d1716
Ilya <ilyastarickov1996@gmail.com>
2021-04-15 19:33:31 +0400

    feat: Add function getNextGeneration and test for it

---
+++ src/ts/gameLogicFunc/getNextGeneration/getNextGeneration.test.ts
@@ -0,0 +1,22 @@
+import { getNextGeneration } from './getNextGeneration'
+
+describe("Testing getNextGeneration function", () => {
+    const state: number[][] = [
+        [1, 1, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+        [1, 1, 1]
+    ]
+
+    const newState: number[][] = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+    ]
+
+    it('Must return field equal newState', () => {
+        console.log(getNextGeneration(state));
+        expect(getNextGeneration(state)).toStrictEqual(newState);
+    })
+})
---
+++ src/ts/gameLogicFunc/getNextGeneration/getNextGeneration.ts
@@ -0,0 +1,22 @@
+import { getNumbAliveCell } from '../getNumbAliveCell/getNumbAliveCell'
+ 
+export function getNextGeneration (state: number[][]): number[][] {
+    return state.map(
+        (row: number[], y) => {
+            return row.map(
+                (cell: number, x) => {
+                    const numberAliveCell: number = getNumbAliveCell(y, x, state);
+                    let cellState;
+                    if (numberAliveCell === 3 && state[y][x] === 0) {
+                        cellState = 1;
+                    } else if ((numberAliveCell === 2 || numberAliveCell === 3) && state[y][x] === 1) {
+                        cellState = 1;
+                    } else {
+                        cellState = 0;
+                    }
+                    return cellState
+                }
+            )
+        }
+    )
+}
