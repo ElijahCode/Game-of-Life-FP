6a45679077cae54a5e8a56a15adf471c8f76a97d
Ilya <ilyastarickov1996@gmail.com>
2021-04-17 15:12:32 +0400

    feat: add onInputChange func and test for it

--- src/ts/utils/utils.ts
+++ src/ts/utils/utils.ts
@@ -3,9 +3,9 @@ export const element: HTMLElement = document.createElement('body');
 element.innerHTML = `
     <body class = "body">
         <table class = "gameField"></table>
-            <div class = "gameControllers">
+        <div class = "gameControllers">
             <input type = "number" class = "input input-width">
-            <input type = "number" class = "input input-width">
+            <input type = "number" class = "input input-heigth">
             <button class = "button button-stopped">Play</button>
         </div>
     </body>
